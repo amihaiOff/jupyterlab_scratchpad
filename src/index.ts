155d5b86abba89d1fad2dbe86b5ce412d02a2731
amihai <amihai.offenbacher@voyantis.ai>
2025-01-11 18:30:49 +0200

    test panel working - now to add a code cell

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,44 @@ import {
 } from '@jupyterlab/application';
 
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
+import { Widget          } from '@lumino/widgets';
+
+
+class SideBarHelloWidget extends Widget {
+  constructor(app: JupyterFrontEnd, commandId: string) {
+    super();
+    this.id = 'sidebar-hello';
+    this.title.label = 'Yo';
+    this.title.closable = true;
+    this.addClass('sbhw');
+    let body = document.createElement('body');
+
+    let button_info: Record<string,string> = {
+      'E' : 'Hello World!',
+      'F' : 'Bonjour le Monde!',
+      'G' : 'Hàlo a Shaoghail!',
+      'H' : '!שלום עולם',
+    } 
+    for (const key in button_info) {
+      let b = document.createElement('button')
+      b.innerText = key;
+      b.addEventListener('click', () => {
+        app.commands.execute(commandId, {greeting:button_info[key]});
+      });
+      body.appendChild(b);
+    }
+    this.node.appendChild(body);
+  }
+}
+
+function _activate(app:     JupyterFrontEnd) {
+    console.log('JupyterLab extension pydjlx is activated!');
+    let commandId = 'pydjlx:Hello';
+    let sbwidget = new SideBarHelloWidget(app, commandId);
+    app.shell.add(sbwidget, 'right');
+    app.shell.activateById(sbwidget.id)
+}
+
 
 /**
  * Initialization data for the jupyterlab_scratchpad extension.
@@ -13,20 +51,21 @@ const plugin: JupyterFrontEndPlugin<void> = {
   description: 'JupyterLab extension for a side panel with a scratchpad',
   autoStart: true,
   optional: [ISettingRegistry],
-  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
-    console.log('JupyterLab extension jupyterlab_scratchpad is activated!');
+  activate: _activate
+  // activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
+  //   console.log('JupyterLab extension jupyterlab_scratchpad is activated!');
 
-    if (settingRegistry) {
-      settingRegistry
-        .load(plugin.id)
-        .then(settings => {
-          console.log('jupyterlab_scratchpad settings loaded:', settings.composite);
-        })
-        .catch(reason => {
-          console.error('Failed to load settings for jupyterlab_scratchpad.', reason);
-        });
-    }
-  }
+  //   if (settingRegistry) {
+  //     settingRegistry
+  //       .load(plugin.id)
+  //       .then(settings => {
+  //         console.log('jupyterlab_scratchpad settings loaded:', settings.composite);
+  //       })
+  //       .catch(reason => {
+  //         console.error('Failed to load settings for jupyterlab_scratchpad.', reason);
+  //       });
+  //   }
+  // }
 };
 
 export default plugin;
